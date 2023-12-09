6dcb363974bf70d4366bd453a66b685a851de584
Frederikbh <frederikbaun@hotmail.dk>
2023-12-09 08:14:44 +0100

    2023/09

---
+++ AdventOfCode/2023/Day09/Solution.cs
@@ -0,0 +1,67 @@
+namespace AdventOfCode.Y2023.Day09;
+
+[ProblemName("Mirage Maintenance")]
+internal class Solution : ISolver 
+{
+
+    public object PartOne(string input) 
+    {
+        var numbers = ParseInput(input);
+
+        return numbers
+            .Select(GetNextInSequence)
+            .Sum();
+    }
+
+    public object PartTwo(string input) 
+    {
+        var numbers = ParseInput(input);
+
+        return numbers
+            .Select(GetPreviousInSequence)
+            .Sum();
+    }
+
+    private static int GetNextInSequence(IReadOnlyList<int> numbers)
+    {
+        if (numbers.All(e => e == 0))
+            return 0;
+
+        var diffs = new List<int>();
+
+        for (var i = 1; i < numbers.Count; i++)
+        {
+            diffs.Add(numbers[i] - numbers[i - 1]);
+        }
+
+        return numbers[^1] + GetNextInSequence(diffs);
+    }
+
+    private static int GetPreviousInSequence(IReadOnlyList<int> numbers)
+    {
+        if (numbers.All(e => e == 0))
+            return 0;
+
+        var diffs = new List<int>();
+
+        for (var i = 1; i < numbers.Count; i++)
+        {
+            diffs.Add(numbers[i] - numbers[i - 1]);
+        }
+
+        return numbers[0] - GetPreviousInSequence(diffs);
+    }
+
+    private static List<List<int>> ParseInput(string input)
+    {
+        var lines = input.Split("\n");
+        var result = new List<List<int>>();
+
+        foreach (var line in lines)
+        {
+            var numbers = line.Split(" ").Select(int.Parse).ToList();
+            result.Add(numbers);
+        }
+        return result;
+    }
+}
