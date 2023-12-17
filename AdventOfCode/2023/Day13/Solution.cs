be94ac86930444d5f42ee7c650b4ddad4d0011d4
Frederikbh <frederikbaun@hotmail.dk>
2023-12-17 12:42:39 +0100

    2023 13,14,15,16

---
+++ AdventOfCode/2023/Day13/Solution.cs
@@ -0,0 +1,106 @@
+namespace AdventOfCode.Y2023.Day13;
+
+[ProblemName("Point of Incidence")]
+public class Solution : ISolver 
+{
+
+    public object PartOne(string input) 
+    {
+        var patterns = ParseInput(input);
+
+        var sum = 0;
+        foreach (var grid in patterns)
+        {
+            var horizontal = FindMirror(grid);
+            var vertical = FindMirror(Rotate(grid));
+
+            sum += vertical;
+            sum += 100 * horizontal;
+        }
+
+        return sum;
+    }
+
+    private static int FindMirror(char[][] grid, int smudges = 0)
+    {
+        for (var i = 0; i < grid.Length - 1; i++)
+        {
+            var diffs = grid[i].Zip(grid[i + 1]).Count(e => e.First != e.Second);
+            if (diffs > smudges)
+                continue;
+
+            // Check if mirror is valid
+            var j = 1;
+            while (i - j >= 0 && i + j + 1 < grid.Length)
+            {
+                diffs += grid[i - j].Zip(grid[i + j + 1]).Count(e => e.First != e.Second);
+
+                if (diffs > smudges)
+                    break;
+                j++;
+            }
+
+            if (diffs == smudges)
+                return i + 1;
+        }
+
+        return 0;
+    }
+
+    private static char[][] Rotate(char[][] grid)
+    {
+        var newGrid = new char[grid[0].Length][];
+
+        for (var i = 0; i < grid[0].Length; i++)
+        {
+            newGrid[i] = new char[grid.Length];
+
+            for (var j = 0; j < grid.Length; j++)
+            {
+                newGrid[i][j] = grid[j][grid[0].Length - i - 1];
+            }
+        }
+
+        return newGrid.Reverse().ToArray();
+    }
+
+    public object PartTwo(string input) 
+    {
+        var patterns = ParseInput(input);
+
+        var sum = 0;
+        foreach (var grid in patterns)
+        {
+            var horizontal = FindMirror(grid, 1);
+            var vertical = FindMirror(Rotate(grid), 1);
+
+            sum += vertical;
+            sum += 100 * horizontal;
+        }
+
+        return sum;
+    }
+
+    private static List<char[][]> ParseInput(string input)
+    {
+        var patterns = new List<char[][]>();
+
+        var splitPatterns = input.Split("\n\n");
+
+        foreach (var splitPattern in splitPatterns)
+        {
+            var lines = splitPattern.Split("\n");
+
+            var grid = new char[lines.Length][];
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                grid[i] = lines[i].ToCharArray();
+            }
+
+            patterns.Add(grid);
+        }
+
+        return patterns;
+    }
+}
---
+++ AdventOfCode/2023/Day16/Solution.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace AdventOfCode.Y2023.Day16;
+
+[ProblemName("The Floor Will Be Lava")]
+public class Solution : ISolver
+{
+
+    public object PartOne(string input)
+    {
+        var grid = input
+            .Split("\n")
+            .Select(x => x.ToCharArray())
+            .ToArray();
+
+        return EnergizeGrid(grid, new Ray(-1, 0, 1, 0));
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        var validStartPoints = new List<Ray>();
+        var grid = input
+            .Split("\n")
+            .Select(x => x.ToCharArray())
+            .ToArray();
+
+        for (var i = 0; i < grid.Length; i++)
+        {
+            validStartPoints.Add(new Ray(i, -1, 0, 1));
+            validStartPoints.Add(new Ray(i, grid[0].Length, 0, -1));
+            validStartPoints.Add(new Ray(-1, i, 1, 0));
+            validStartPoints.Add(new Ray(grid.Length, i, -1, 0));
+        }
+
+        EnergizeGrid(grid, new Ray(10, 9, -1, 0));
+
+        var energizeResults = validStartPoints
+            .Select(e => EnergizeGrid(grid, e))
+            .ToList();
+
+        return energizeResults.Max();
+    }
+
+    private static int EnergizeGrid(char[][] grid, Ray startPoint)
+    {
+        var seenRays = new HashSet<Ray>();
+        var energizedTiles = new HashSet<Point>();
+
+        var rays = new Queue<Ray>();
+        rays.Enqueue(startPoint);
+
+        while (rays.Count > 0)
+        {
+            var ray = rays.Dequeue();
+
+            var location = new Point(ray.X + ray.DX, ray.Y + ray.DY);
+
+            if (location.X < 0 || location.X >= grid[0].Length || location.Y < 0 ||
+                location.Y >= grid.Length)
+            {
+                continue;
+            }
+
+            energizedTiles.Add(location);
+
+            var tile = grid[location.Y][location.X];
+
+            var nextRays = new List<Ray>();
+            if (tile is '.')
+            {
+                nextRays.Add(new Ray(location.X, location.Y, ray.DX, ray.DY));
+
+            }
+            else if (tile == '\\')
+            {
+                if (ray.DX > 0)
+                {
+                    nextRays.Add(new Ray(location.X, location.Y, 0, 1));
+                }
+                else if (ray.DX < 0)
+                {
+                    nextRays.Add(new Ray(location.X, location.Y, 0, -1));
+                }
+                else if (ray.DY > 0)
+                {
+                    nextRays.Add(new Ray(location.X, location.Y, 1, 0));
+                }
+                else
+                {
+                    nextRays.Add(new Ray(location.X, location.Y, -1, 0));
+                }
+            }
+            else if (tile == '/')
+            {
+                if (ray.DX > 0)
+                {
+                    nextRays.Add(new Ray(location.X, location.Y, 0, -1));
+                }
+                else if (ray.DX < 0)
+                {
+                    nextRays.Add(new Ray(location.X, location.Y, 0, 1));
+                }
+                else if (ray.DY > 0)
+                {
+                    nextRays.Add(new Ray(location.X, location.Y, -1, 0));
+                }
+                else
+                {
+                    nextRays.Add(new Ray(location.X, location.Y, 1, 0));
+                }
+            }
+            else if (tile == '|')
+            {
+                if (ray.DY != 0)
+                {
+                    nextRays.Add(new Ray(location.X, location.Y, ray.DX, ray.DY));
+                }
+                else
+                {
+                    nextRays.Add(new Ray(location.X, location.Y, 0, 1));
+                    nextRays.Add(new Ray(location.X, location.Y, 0, -1));
+                }
+            }
+            else if (tile == '-')
+            {
+                if (ray.DX != 0)
+                {
+                    nextRays.Add(new Ray(location.X, location.Y, ray.DX, ray.DY));
+                }
+                else
+                {
+                    nextRays.Add(new Ray(location.X, location.Y, 1, 0));
+                    nextRays.Add(new Ray(location.X, location.Y, -1, 0));
+                }
+            }
+
+            foreach (var nextRay in nextRays)
+            {
+                if (seenRays.Add(nextRay))
+                {
+                    rays.Enqueue(nextRay);
+                }
+            }
+        }
+
+        return energizedTiles.Count;
+    }
+
+    private record struct Ray(int X, int Y, int DX, int DY);
+
+    private record struct Point(int X, int Y);
+}
